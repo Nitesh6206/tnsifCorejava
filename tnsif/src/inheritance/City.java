d8ad6e15cab1a3fc8a9995de035db8433c83c8ea
win10 <win10@NITESH>
2024-01-23 23:16:52 +0530

    commit.

--- tnsif/src/inheritance/City.java
+++ tnsif/src/inheritance/City.java
@@ -1,5 +1,28 @@
 package inheritance;
 
-public class City {
+public class City extends State {
+	private String cityName;
+	private float area;
+	
+	public String getCityName() {
+		return cityName;
+	}
 
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public float getArea() {
+		return area;
+	}
+
+	public void setArea(float area) {
+		this.area = area;
+	}
+
+	@Override
+	public String toString() {
+		return "City [cityName=" + cityName + ", area=" + area + ", StateName=" + getStateName() + ", Language= "
+				+ getLanguage() + ", CountryName=" + getCountryName() + ", Capital=" + getCapital() + "]";
+	}
 }
